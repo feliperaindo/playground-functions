8b4eb16b10a867cd4e8a6c29679aec0464d1c910
feliperaindo <felipe.raindo@gmail.com>
2022-08-26 21:08:05 -0300

    Resolve lint encode - falta o decoder.

--- src/challenges.js
+++ src/challenges.js
@@ -101,25 +101,49 @@ function fizzBuzz(array) {
 console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
-function encode(string) {
-  let encoder = [
+function arrayCreator(string) {
+  let array = [];
+  for (const i of string) {
+    array.push(i);
+  }
+  return array;
+}
+
+function encoder(string) {
+  let codifier = [
     { number: 'a', crypto: 1 }, { number: 'e', crypto: 2 }, { number: 'i', crypto: 3 },
     { number: 'o', crypto: 4 }, { number: 'u', crypto: 5 },
   ];
-  let phraseEncoded = '';
+
+  for (let key of codifier) {
+    if (string === key.number) {
+      string = key.crypto;
+      return string;
+    }
+  }
+  return string;
+}
+
+function arrayEncoder(array) {
   let arrayEncoded = [];
 
-  for (const i in string) {
-    arrayEncoded.push(string[i]);
-    for (let ii in encoder) {
-      if (string[i] === encoder[ii].number) {
-        arrayEncoded[i] = encoder[ii].crypto;
-      }
-    }
-    phraseEncoded += arrayEncoded[i];
+  for (let i of array) {
+    arrayEncoded.push(encoder(i));
+  }
+  return arrayEncoded;
+}
+
+function encode(string) {
+  let array = arrayCreator(string);
+  let arrayEncoded = arrayEncoder(array);
+  let phraseEncoded = '';
+
+  for (let i of arrayEncoded) {
+    phraseEncoded += i;
   }
   return phraseEncoded;
 }
+console.log(encode('vamos nessa'));
 
 function decode(string) {
   let decoder = [
--- src/teste.js
+++ src/teste.js
@@ -1,26 +1,33 @@
-// eslint-disable-next-line padded-blocks
 function arrayCreator(string) {
-
   let array = [];
   for (const i of string) {
     array.push(i);
   }
   return array;
 }
-function arrayEncoder(array) {
-  let encoder = [
+
+function encoder(string) {
+  let codifier = [
     { number: 'a', crypto: 1 }, { number: 'e', crypto: 2 }, { number: 'i', crypto: 3 },
     { number: 'o', crypto: 4 }, { number: 'u', crypto: 5 },
   ];
 
-  for (let i of array) {
-    for (let key of encoder) {
-      if (i === key.number) {
-        i = key.crypto;
-      }
+  for (let key of codifier) {
+    if (string === key.number) {
+      string = key.crypto;
+      return string;
     }
   }
-  return array;
+  return string;
+}
+
+function arrayEncoder(array) {
+  let arrayEncoded = [];
+
+  for (let i of array) {
+    arrayEncoded.push(encoder(i));
+  }
+  return arrayEncoded;
 }
 
 function encode(string) {
