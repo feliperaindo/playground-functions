1a26accf8e0ab9b2c307aa9dbe9edf8a5de6cf75
feliperaindo <felipe.raindo@gmail.com>
2022-08-29 14:13:25 -0300

    resolve questão hydrate

--- src/challenges2.js
+++ src/challenges2.js
@@ -9,9 +9,19 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+  let arrayNumbers = string.match(/\d+/g);
+  let result = 0;
+
+  for (const i of arrayNumbers) {
+    result += parseInt(i, 0);
+  }
+  if (result > 1) {
+    return `${result} copos de água`;
+  }
+  return `${result} copo de água`;
 }
+console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'));
 
 module.exports = {
   generatePhoneNumber,
